7a4abc7afd949e96920c3112a167a5ca11c3dfeb
mhamed <mohamed.hamed@zalando.de>
2020-02-09 16:51:45 +0100

    feat(exercise): added table of movies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Movies from "./components/Movies";
 
 function App() {
   return (
-   <main className="container">
-     <h1>Hello World</h1>
-   </main>
+    <main className="container">
+      <Movies/>
+    </main>
   );
 }
 
